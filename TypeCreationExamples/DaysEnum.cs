21f2d98d03d5b189ea482460dbc4860f9ca52d18
Sinan NAR <sinan.nar@outlook.com>
2017-12-03 18:30:04 +1300

    Add project files.

---
+++ TypeCreationExamples/DaysEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeCreationExamples
+{
+    enum Days : byte { Sat = 1, Sun, Mon, Tue, Wed, Thu, Fri }
+}
+
+namespace TypeCreationSecondWayExamples
+{
+    [Flags]
+    enum Days
+    {
+        None = 0x0,
+        Sun = 0x1,
+        Mon = 0x2,
+        Tue = 0x4,
+        Wed = 0x8,
+        Thu = 0x10,
+        Fri = 0x20,
+        Sat = 0x40,
+    } 
+}
---
+++ TypeCreationExamples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TypeCreationExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    } 
+}
+/*
+ 
+    In C#, you can have Value Types, Reference Types and Value types. Pointer types are rarely used.
+     
+    Enum is special kind of value type in C#
+
+     
+*/
