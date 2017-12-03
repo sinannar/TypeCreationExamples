6c2f3c254a7b64e4cadb4e66677a44913374a877
Sinan NAR <sinan.nar@outlook.com>
2017-12-03 18:43:34 +1300

    Struct has been added as an example

---
+++ TypeCreationExamples/PointStruct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeCreationExamples
+{
+    public struct Point
+    {
+        public int x, y;
+
+        public Point(int p1, int p2)
+        {
+            x = p1;
+            y = p2;
+        }
+
+    }
+}
+
+/*
+
+    Structs and classes can have methods, fields, properties, constructors and other functionalities. you can not, however, decalare your own empty constructor for Struct.
+    Also structs can not be used in an inheritance hieararchy
+
+*/
--- TypeCreationExamples/Program.cs
+++ TypeCreationExamples/Program.cs
@@ -16,5 +16,12 @@ namespace TypeCreationExamples
      
     Enum is special kind of value type in C#
 
-     
+    There are two location in which a type can be stored in memory, the heap and the stack. The value of a reference type is stored on the heap and the adress to this value is stored on stack. 
+    Althought a value type is normally stored on the stack, there are exceptions, but the this is general idea. For the exceptions, a class that contains a value type as one of its fields, a lambda expression that closes
+    over a value type, or a value type that is boxed can be shown. Benefit of storing data on stack is that its faster, smaller and does not need the attention of garbage collector.
+
+    All objects in C$ inherit from System.Object. Value Types however, inherit from System.ValueType which inherits from System.Object and override default functions such as GetHashCode,
+    Equals,, ToString etc to give them more meaningful implementation for a value type. In C# you can not directly inherit from Sysyem.ValueType, instead, you can use the struct keyword to create a new value type.
+
+    
 */
